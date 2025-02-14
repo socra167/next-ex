c8686d56c8929b9c56029d2165cd5ddbbc1fe3d0
socra167 <socra167@gmail.com>
2025-02-14 15:03:05 +0900

    레이아웃에 내정보 링크 추가

--- frontend/src/app/ClientLayout.tsx
+++ frontend/src/app/ClientLayout.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 
-export default function ClientLayout({
+export default function ClinetLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
@@ -15,9 +15,12 @@ export default function ClientLayout({
           <Link href="/about">소개</Link>
           <Link href="/post/list">글 목록</Link>
           <Link href="/member/login">로그인</Link>
+          <Link href="/member/me">내정보</Link>
         </header>
         <div className="flex-grow">{children}</div>
         <footer>푸터</footer>
+
+        <input type="text" />
       </body>
     </html>
   );
