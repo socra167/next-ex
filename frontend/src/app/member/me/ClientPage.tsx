256af1b6bd6a7b36731627ad387311fcfb5b37e0
socra167 <socra167@gmail.com>
2025-02-14 15:01:20 +0900

    서버 컴포넌트에서 fetch, 편함, 대신 브라우저에서 받아온 쿠키를 받아서 백엔드로 fetch할 때 적용해야 함

--- frontend/src/app/member/me/ClientPage.tsx
+++ frontend/src/app/member/me/ClientPage.tsx
@@ -1,36 +1,12 @@
 "use client";
 
 import { components } from "@/lib/backend/apiV1/schema";
-import client from "@/lib/backend/client";
-import { useEffect, useState } from "react";
 
-export default function ClinetPage() {
-  const [me, setMe] = useState<components["schemas"]["MemberDto"] | null>(null);
-
-  async function getMe() {
-    const response = await client.GET("/api/v1/members/me", {
-      credentials: "include",
-    });
-
-    if (response.error) {
-      alert(response.error.msg);
-      return;
-    }
-
-    const rsData = response.data;
-    const memberDto = rsData.data;
-
-    setMe(memberDto);
-  }
-
-  useEffect(() => {
-    getMe();
-  }, []);
-
-  if (me == null) {
-    return <div>로딩중 .. </div>;
-  }
-
+export default function ClinetPage({
+  me,
+}: {
+  me: components["schemas"]["MemberDto"];
+}) {
   return (
     <>
       <div>내정보 페이지</div>
--- frontend/src/app/member/me/page.tsx
+++ frontend/src/app/member/me/page.tsx
@@ -1,5 +1,19 @@
+import client from "@/lib/backend/client";
 import ClientPage from "./ClientPage";
+import { cookies } from "next/headers";
 
 export default async function Page() {
-  return <ClientPage />;
+  const response = await client.GET("/api/v1/members/me", {
+    headers: {
+      cookie: (await cookies()).toString(),
+    },
+  });
+
+  if (response.error) {
+    return <div>{response.error.msg}</div>;
+  }
+
+  const memberDto = response.data.data;
+
+  return <ClientPage me={memberDto} />;
 }
