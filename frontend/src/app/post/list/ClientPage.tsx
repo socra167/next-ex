a6b8b3681fcac120608e8885881317351435b154
socra167 <socra167@gmail.com>
2025-02-14 10:02:07 +0900

    클라이언트 컴포넌트, 서버 컴포넌트 분리

---
+++ frontend/src/app/post/list/ClientPage.tsx
@@ -0,0 +1,83 @@
+import { components } from "@/lib/backend/apiV1/schema";
+import Link from "next/link";
+
+export default async function ClientPage({
+  rsData,
+  keywordType,
+  keyword,
+  pageSize,
+  page,
+}: {
+  rsData: components["schemas"]["RsDataPageDto"];
+  keywordType?: "title" | "content";
+  keyword: string;
+  pageSize: number;
+  page: number;
+}) {
+  const pageDto = rsData.data;
+
+  return (
+    <div>
+      <h1>글 목록</h1>
+
+      <div>응답 코드: {rsData.code}</div>
+      <div>결과 메시지: {rsData.msg}</div>
+
+      <div>totalPages: {pageDto.totalPages}</div>
+      <div>totalItems: {pageDto.totalItems}</div>
+      <div>currentPageNo: {pageDto.currentPageNo}</div>
+      <div>pageSize: {pageDto.pageSize}</div>
+
+      <hr />
+
+      <form>
+        <select name="keywordType">
+          <option value="title">제목</option>
+          <option value="content">내용</option>
+        </select>
+        <input
+          placeholder="검색어 입력"
+          type="text"
+          name="keyword"
+          defaultValue={keyword}
+        ></input>
+        <input type="submit" value="검색"></input>
+        <label className="ml-2">페이지당 행 개수</label>
+        <select name="pageSize" defaultValue="30">
+          <option value="10">10</option>
+          <option value="30">30</option>
+          <option value="50">50</option>
+        </select>
+      </form>
+      <div className="flex gap-3">
+        {Array.from({ length: pageDto.totalPages }, (_, i) => i + 1).map(
+          (pageNo) => {
+            return (
+              <Link
+                key={pageNo}
+                className={pageNo == page ? `text-blue-500` : ""}
+                href={`/post/list?keywordType=${keywordType}&keyword=${keyword}&pageSize=${pageSize}&page=${pageNo}`}
+              >
+                {pageNo}
+              </Link>
+            );
+          }
+        )}
+      </div>
+      <ul>
+        {pageDto.items.map((item) => {
+          return (
+            <li className="border-2 border-red-500 my-2 p-2" key={item.id}>
+              <div>id: {item.id}</div>
+              <div>title: {item.title}</div>
+              <div>authorId: {item.authorId}</div>
+              <div>authorName: {item.authorName}</div>
+              <div>published: {`${item.published}`}</div>
+              <div>listed: {`${item.listed}`}</div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
--- frontend/src/app/post/list/page.tsx
+++ frontend/src/app/post/list/page.tsx
@@ -1,6 +1,8 @@
 import { components, paths } from "@/lib/backend/apiV1/schema";
 import Link from "next/link";
 import createClient from "openapi-fetch";
+import ClientPage from "./ClientPage";
+import { pages } from "next/dist/build/templates/app-page";
 
 const client = createClient<paths>({
   baseUrl: "http://localhost:8080",
@@ -37,69 +39,13 @@ export default async function Page({
   const rsData = response.data!!;
   const pageDto = rsData.data;
 
-  pageDto.items;
-
   return (
-    <div>
-      <h1>글 목록</h1>
-
-      <div>응답 코드: {rsData.code}</div>
-      <div>결과 메시지: {rsData.msg}</div>
-
-      <div>totalPages: {pageDto.totalPages}</div>
-      <div>totalItems: {pageDto.totalItems}</div>
-      <div>currentPageNo: {pageDto.currentPageNo}</div>
-      <div>pageSize: {pageDto.pageSize}</div>
-
-      <hr />
-
-      <form>
-        <select name="keywordType">
-          <option value="title">제목</option>
-          <option value="content">내용</option>
-        </select>
-        <input
-          placeholder="검색어 입력"
-          type="text"
-          name="keyword"
-          defaultValue={keyword}
-        ></input>
-        <input type="submit" value="검색"></input>
-        <label className="ml-2">페이지당 행 개수</label>
-        <select name="pageSize" defaultValue="30">
-          <option value="10">10</option>
-          <option value="30">30</option>
-          <option value="50">50</option>
-        </select>
-      </form>
-      <div className="flex gap-3">
-        {Array.from({ length: pageDto.totalPages }, (_, i) => i + 1).map(
-          (pageNo) => {
-            return (
-              <Link
-                className={pageNo == page ? `text-blue-500` : ""}
-                href={`/post/list?keywordType=${keywordType}&keyword=${keyword}&pageSize=${pageSize}&page=${pageNo}`}
-              >
-                {pageNo}
-              </Link>
-            );
-          }
-        )}
-      </div>
-      <ul>
-        {pageDto.items.map((item) => {
-          return (
-            <li className="border-2 border-red-500 my-2 p-2" key={item.id}>
-              <div>id: {item.id}</div>
-              <div>title: {item.title}</div>
-              <div>authorId: {item.authorId}</div>
-              <div>authorName: {item.authorName}</div>
-              <div>published: {`${item.published}`}</div>
-              <div>listed: {`${item.listed}`}</div>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+    <ClientPage
+      rsData={rsData}
+      pageSize={pageSize}
+      keyword={keyword}
+      keywordType={keywordType}
+      page={page}
+    />
   );
 }
