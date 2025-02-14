7e2d686793d55f360bf56183947acfdb5ba5ca2b
socra167 <socra167@gmail.com>
2025-02-14 15:11:30 +0900

    로그아웃 구현

--- frontend/src/app/ClientLayout.tsx
+++ frontend/src/app/ClientLayout.tsx
@@ -1,12 +1,16 @@
 "use client";
 
 import Link from "next/link";
+import client from "./client";
+import { useRouter } from "next/navigation";
 
 export default function ClinetLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const router = useRouter();
+
   return (
     <html lang="en">
       <body className="min-h-[100dvh] flex flex-col">
@@ -15,6 +19,24 @@ export default function ClinetLayout({
           <Link href="/about">소개</Link>
           <Link href="/post/list">글 목록</Link>
           <Link href="/member/login">로그인</Link>
+          <Link
+            href=""
+            onClick={async (e) => {
+              e.preventDefault();
+              const response = await client.DELETE("/api/v1/members/logout", {
+                credentials: "include",
+              });
+
+              if (response.error) {
+                alert(response.error.msg);
+                return;
+              }
+
+              router.push("/post/list");
+            }}
+          >
+            로그아웃
+          </Link>
           <Link href="/member/me">내정보</Link>
         </header>
         <div className="flex-grow">{children}</div>
