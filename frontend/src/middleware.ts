d67105016fe9c07b7f334e2dc90d3b55b65f5fcf
socra167 <socra167@gmail.com>
2025-02-17 11:52:09 +0900

    middleware로 서버에서 받은 응답 set-cookie를 next응답에도 적용

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,11 +1,24 @@
-import type { NextRequest } from "next/server";
+import { cookies } from "next/headers";
+import { NextResponse, type NextRequest } from "next/server";
+import client from "./app/client";
 
-// This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-  console.log("middleware");
+export async function middleware(request: NextRequest) {
+  const reqToken = request.cookies.get("accessToken");
+  // 스프링부트 서버가 준 쿠키를 nextResponse에 심어주면 된다.
+  const nextResponse = NextResponse.next(); // NextJS의 Response
+
+  // 스프링부트 서버에 한번 더 내정보 조회 요청
+  const response = await client.GET("/api/v1/members/me", {
+    headers: {
+      cookie: (await cookies()).toString(),
+    },
+  });
+
+  // 스프링부트 서버에서 받은 응답 set-cookie를 next응답으로 설정
+  const springCookie = response.response.headers.getSetCookie();
+  nextResponse.headers.set("set-cookie", String(springCookie));
 }
 
-// See "Matching Paths" below to learn more
 export const config = {
   matcher: "/:path*",
 };
