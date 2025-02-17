8ca1e19c8ec716fba88aa3c7eb0f40aadf28ff6e
socra167 <socra167@gmail.com>
2025-02-17 11:28:33 +0900

    middleware.ts 생성

---
+++ frontend/src/middleware.ts
@@ -0,0 +1,11 @@
+import type { NextRequest } from "next/server";
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  console.log("middleware");
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: "/:path*",
+};
