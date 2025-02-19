feac195779674424ffcebbc7f30fe3bc0ab2758f
socra167 <socra167@gmail.com>
2025-02-19 09:50:25 +0900

    ShadCN 버튼 컴포넌트 사용해보기

--- frontend/src/app/ClientLayout.tsx
+++ frontend/src/app/ClientLayout.tsx
@@ -4,9 +4,6 @@ import Link from "next/link";
 import client from "./client";
 import { useRouter } from "next/navigation";
 import { components } from "@/lib/backend/apiV1/schema";
-import { config } from "@fortawesome/fontawesome-svg-core";
-import "@fortawesome/fontawesome-svg-core/styles.css";
-config.autoAddCss = false;
 
 export default function ClinetLayout({
   children,
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,16 @@
+import { Button } from "@/components/ui/button";
+
 export default function Page() {
-  return <div>메인 페이지입니다.</div>;
+  return (
+    <>
+      <div>메인 페이지입니다.</div>
+      <button>button</button>
+      <Button variant="outline">Button</Button>
+      <Button variant="destructive">Button</Button>
+      버튼에 테일윈드 CSS도 적용 가능하다.
+      <Button className="bg-yellow-300 w[100px]" variant="outline">
+        Button
+      </Button>
+    </>
+  );
 }
